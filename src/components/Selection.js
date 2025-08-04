068de5e64012d08185e8d097828644efcabe7406
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-08-04 21:05:13 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,22 @@
 
+import React, { useState } from "react";
+
+const Selection = ({ applyColor }) => {
+  const [bgStyle, setBgStyle] = useState({});
+
+  const handleClick = () => {
+    applyColor(setBgStyle);
+  };
+
+  return (
+    <div
+      className="fix-box"
+      style={bgStyle}
+      onClick={handleClick}
+    >
+      {/* You can leave this empty or add some text like "Click me" */}
+    </div>
+  );
+};
+
+export default Selection;
